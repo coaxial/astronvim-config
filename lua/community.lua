2d81b14292cf850e3a04d9188d0e05c059da9f1c
Rock <coaxial@users.noreply.github.com>
2025-09-09 13:23:02 +0200

    Configure TS language server and CPP (#11)
    * Add CPP
    
    * Configure TS

--- lua/community.lua
+++ lua/community.lua
@@ -11,4 +11,6 @@ return {
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
