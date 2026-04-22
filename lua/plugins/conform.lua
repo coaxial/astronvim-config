282aaf242430e7dadf6bebf09aec1f72556d17df
coaxial <c@64b.it>
2026-04-22 09:29:42 +0200

    Add default formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,23 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      yaml = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      less = { "prettier" },
+      html = { "prettier" },
+      markdown = { "prettier" },
+      mdx = { "prettier" },
+      vue = { "prettier" },
+      svelte = { "prettier" },
+      astro = { "prettier" },
+      graphql = { "prettier" },
     },
     format_on_save = {
       timeout_ms = 1000,
